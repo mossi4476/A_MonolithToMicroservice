230d4661d8ef616e9ce7de2cda5935e13bd8d3b1
DungNguyen <tiendungnguyen.4476@gmail.com>
2024-09-28 23:09:37 +0700

    refactor api and frontend

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a4101532a917e4788b83e332b5cf334c-1935198166.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
